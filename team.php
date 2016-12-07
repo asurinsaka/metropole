ff4e77d39bb67b33302ef4672b11ad35b02e9593
Asurin <qiaozha@gmail.com>
2016-12-06 19:07:30 -0500

    format team page

--- team.php
+++ team.php
@@ -37,292 +37,75 @@
 
     </div>
 
-    <div class="well">
+    <?php
+      function listTeamMembers($country){
+        $row = 1;
+        if (($handle = fopen("files/team.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $num = count($data);
+                // echo "<p> $num fields in line $row: <br /></p>\n";
+                // $row++;
+                // for ($c=0; $c < $num; $c++) {
+                //     echo $data[$c] . "<br />\n";
+                // }
+                if ($data[0] == $country){
+                  echo '<div class="row" >';
+                  echo '  <div class="col-sm-4 vcenter">';
+
+                  if (file_exists ('image/'.$data[1].'.jpg' )){
+                      echo '  <img src="image/' . $data[1] . '.jpg" alt="Team image" class="img-responsive">';
+                  }elseif (file_exists ('image/'.$data[1].'.png')){
+                      echo '  <img src="image/' . $data[1] . '.png" alt="Team image" class="img-responsive">';
+                  }
+                  echo "  </div>";
+                  echo '  <div class="col-sm-8 vcenter">';
+                  echo '    <h4 id="name">' . $data[1] .' ' . $data[2];
+                  echo '    <a href="mailto:' . $data[3] . '" type="button" class="btn btn-default btn-xs" aria-label="email">';
+                  echo '      <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>';
+                  echo "    </a>";
+                  echo '    </h4>';
+                  echo '    <h5 id="organization">' . $data[4] . "</h5>";
+                  // echo '    <p><strong>Project Role:</strong> '. $data[5] . '</p>';
+                  // echo '    <p><strong>Core Expertise and Interests:</strong> '. $data[6] . '</p>';
+                  echo '    <p> '. $data[5] . '</p>';
+                  echo '    <p> '. $data[6] . '</p>';
+                  echo "  </div>";
+                  echo "</div>";
+                }
+            }
+            fclose($handle);
+        }
+      }
+
+    ?>
+    <div class="well" id="team">
       <h2>Research Team Members </h2>
 
-      <h3 id="brazil">Brazil </h3>
-      <div class="row">
-        <div class="col-md-4">
-          <a href="http://www.bv.fapesp.br/en/pesquisador/741/jose-antonio-marengo-orsini/">
-          <h4>Co-Investigator, Jose Marengo Orsini, Dr.  Centro Nacional de Monitoramento e Alertas de Desastres Naturais (CEMADEN) </h4>
-          <ul>
-            <li>
-              Role in grant: Lead PPI, Brazil; generating high-resolution climate and SLR scenarios
-            </li>
-            <li>
-              Core expertise and interests: tropical climatology, climate prediction and observation/modeling of climate change, vulnerability and impact assessments
-            </li>
-          </ul>
-        </a>
+      <div class="row" id="country">
+        <div class="col-md-12">
+          <h3 id="us">United States of America  </h3>
         </div>
+      </div>
+      <?php listTeamMembers("US") ?>
 
-        <div class="col-md-4">
-          <a href="http://www.bv.fapesp.br/en/pesquisador/89827/luci-hidalgo-nunes/">
-          <h4>Co-investigator, Luci Hidalgo Nunes, Professor, MS-5, University of Campinas (IG- UNICAMP) Institute of Geosciences </h4>
-          <ul>
-            <li>
-              Role in grant: define and evaluate extreme events; integrate, conduct and coordinate sociological research with Brazil team
-            </li>
-            <li>
-              Core expertise and interests: extreme climatic events and impacts in urban centers, risk perception and role of media in providing climate science information to the public
-            </li>
-          </ul>
-        </a>
+
+
+      <div class="row" id="country">
+        <div class="col-md-12">
+          <h3 id="uk">United Kingdom  </h3>
         </div>
-        <div class="col-md-4">
-          <a href="http://www.inpe.br/ingles/">
-          <h4>Co-investigator, Luiz Aragão, Dr. Adjunct Scientist, Earth Observation, National Institute for Space Research (INPE)</h4>
-          <ul>
-            <li>
-              General Coordination, Remote Sensing Division
-            </li>
-            <li>
-              Role in grant: PULSE platform implementation and application in case studies
-            </li>
-            <li>
-              Core expertise and interests: tropical environmental changes focusing on impacts of
-              extreme drought events in Amazonia
-            </li>
-          </ul>
-        </a>
+      </div>
+      <?php listTeamMembers("UK") ?>
+
+      <div class="row" id="country">
+        <div class="col-md-12">
+          <h3 id="brazil">Brazil </h3>
         </div>
       </div>
 
-      <div class="row">
-        <div class="col-md-4">
-          <a href="http://www.inpe.br/ingles/">
-          <h4>Co-investigator , Sin Chan Chou, Dr. Research Scientist III, Center for Weather Forecasts and Climate Studies (CPTEC), National Institute for Space Research (INPE)</h4>
-          <ul>
-            <li>
-              Role in grant: downscaling of climate change scenarios
-            </li>
-            <li>
-              Core expertise and interests: climate modeling and development, Eta model, meterological events and landslides
-            </li>
-          </ul>
-        </a>
-        </div>
+      <?php listTeamMembers("Brazil") ?>
 
-        <div class="col-md-4">
-          <h4>Celia Regina de Gouveia Souza, Dr. Scientific Research VI, Instituto Geologico-Sao Paulo</h4>
-          <ul>
-            <li>
-              Role in grant: evaluate areas under risk of floods and storm surges
-            </li>
-            <li>
-              Core expertise and interests:  coastal geomorphology; geomorphology and biogeography of coastal systems; coastal management; prevention to natural disasters
-            </li>
-          </ul>
-        </div>
 
-        <div class="col-md-4">
-          <h4>Joseph Harari, Professor, University of Sao Paulo, Institute of Oceanography</h4>
-          <ul>
-            <li>
-              Role in grant: generate scenarios of sea level rise
-            </li>
-            <li>
-              Core expertise and interests: coastal circulation; hydrodynamic and dispersion modeling, analyses of time series; long term climate change and oceanographic sea level monitoring, evaluation of satellite altimetry
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-4">
-          <a href="http://www.ime.unicamp.br/~js/geophys.html">
-            <h4>Roberto Greco, Dr. University of Campinas (IG- UNICAMP) Institute of Geosciences</h4>
-            <ul>
-              <li>
-                Role in grant: sociological research, stakeholder engagement, facilitator for COAST public engagement meetings
-              </li>
-              <li>
-                Core expertise and interests: environment, resources and cultural heritage
-              </li>
-            </ul>
-          </a>
-        </div>
-
-        <div class="col-md-4">
-          <a href="http://www.inpe.br/ingles/">
-            <h4>Lincoln Muniz Alves, Dr., Earth Observation, National Institute for Space Research (INPE) </h4>
-            <ul>
-              <li>
-                Role in grant: define and evaluate extreme events, modelling of climate change
-              </li>
-              <li>
-                Core expertise and interests: climate change, numerical modelling
-              </li>
-            </ul>
-          </a>
-        </div>
-
-        <div class="col-md-4">
-          <h4>Eduardo Kimoto Hosokawa, Engineer, Santos City Hall, Department of Urban Development</h4>
-          <ul>
-            <li>
-              Role in grant: collect information for COAST process, stakeholder engagement
-            </li>
-            <li>
-              Core expertise and interests: geotechnology for planning urban process
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-4">
-          <h4>Ernesto Kasuwo Tabuchi, Engineer, Santos City Hall, Department of Environment</h4>
-          <ul>
-            <li>
-              Role in grant: collect information for COAST process, stakeholder engagement
-            </li>
-            <li>
-              Core expertise and interests: coastal vulnerability to climate change, disaster risk and risk management
-            </li>
-          </ul>
-        </div>
-      </div>
-
-    <h3 id="uk">United Kingdom  </h3>
-    <div class="row">
-      <div class="col-md-4">
-        <h4>Co-InvestigatorMark Pelling, Professor, King’s College of London, Department of Geography</h4>
-        <ul>
-          <li>
-            Role in grant: conceptualization of the Adaptive Capacity Index assessment and implementation in UK case study
-          </li>
-          <li>
-            Core expertise and experience: social and institutional components of resilience, transformation and adaptation to climate change and natural hazards, disaster risk and risk management
-          </li>
-        </ul>
-      </div>
-      <div class="col-md-4">
-        <h4> Christine Susan Betham, Chair and Professor </h4>
-        <ul>
-          <li>
-            Role in grant: climate modelin
-          </li>
-          <li>
-            Core expertise and experience: biophysical processes involved in generation of urban climates, developing and evaluating numerical urban land surface models
-          </li>
-        </ul>
-      </div>
-      <div class="col-md-4">
-        <h4>Shona Patterson, Postdoctoral Research Associate</h4>
-        <ul>
-          <li>
-            Role in grant: Senior Personnel, conduct interviews for Adaptive Capacity Index in UK and US study sites, analysis of ACI and coordinator for Selsey Sea Defence Conference
-          </li>
-          <li>
-            Core expertise and experience:   coastal resource management, climate adaptation and adaptive capacity in coastal areas, governance, social justice, and partnership building.
-          </li>
-        </ul>
-      </div>
-
-    </div>
-
-    <h3 id="us">United States of America  </h3>
-    <div class="row">
-      <div class="col-md-4">
-        <h4>Project PIDr. Frank Muller-Karger, Professor USF College of Marine Science, Institute for Marine Remote Sensing (IMARS)</h4>
-        <ul>
-          <li>
-            Role in grant: PI, collecting and analyzing large scale data sets, overall grant management
-          </li>
-          <li>
-            Core expertise and interests: biological oceanography, satellite remote sensing, national/international coastal and marine policy
-          </li>
-        </ul>
-      </div>
-
-      <div class="col-md-4">
-        <h4>CJ Reynolds, Research Associate</h4>
-        <ul>
-          <li>
-            Role in grant: Project Coordinator
-          </li>
-          <li>
-            Core expertise and interests: stakeholder engagement, risk communication, survey development, partnership development, professional continuing education, and municipal planning and finance policy.
-          </li>
-        </ul>
-      </div>
-      <div class="col-md-4">
-        <a href="http://hosted.usf.edu/rhetcomp/?page_id=3596&viewuser=18">
-          <h4>Karen Langbehn, Ph.D. </h4>
-          <ul>
-            <li>
-              Role in grant: Research Assistant
-            </li>
-            <li>
-              Core expertise and interests: rhetoric of science policy, stakeholder engagement in science policy, decision making under uncertainty
-            </li>
-          </ul>
-        </a>
-      </div>
-    </div>
-
-      <div class="row">
-        <div class="col-md-4">
-          <h4> Hannah Torres, Ph.D. Candidate</h4>
-          <ul>
-            <li>
-              Role in grant: Research Assistant
-            </li>
-            <li>
-              Core expertise and interests: Natural resource policy and management, community resilience and climate adaptation, human and environmental geography.
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-4">
-          <h4>Jack Kartez, USEPA Region 1 New England Environmental Finance Center </h4>
-          <ul>
-            <li>
-              Role in grant: Senior Personnel, survey design and analysis, risk communications advisor
-            </li>
-            <li>
-              Core expertise and interests: environmental hazards, climate adaptation planning, survey research methods, collaborative planning and environmental mediation
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-4">
-          <h4>Co-Investigator Sam Merrill, Senior Practice Leader, GEI Consultants </h4>
-          <ul>
-            <li>
-              Role in grant: Co-Investigator, define experimental design and technical and community-based implementation of COAST process
-            </li>
-            <li>
-              Core expertise and interests: cost-benefit analysis of regional adaptation actions, stakeholder engagement in adaptation planning processes
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-4">
-          <h4>J.T. Lockman, Senior Project Manager, GEI Consultants</h4>
-          <ul>
-            <li>
-              Role in grant: Senior Personnel, Facilitator for COAST public engagement meetings
-            </li>
-            <li>
-              Core expertise and interests: over 25 years of experience in municipal codes, town and city management, land use and capital improvements planning
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-4">
-          <h4>American Planning Association – Florida, Alex Magee, Executive Director</h4>
-          <ul>
-            <li>
-              Role in grant:
-            </li>
-            <li>
-              Core expertise and interests:
-            </li>
-          </ul>
-
-        </div>
-      </div>
     </div>
 
     </div>
