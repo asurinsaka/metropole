20cbeffd297b0839d123b326179133d67edd901a
Asurin <qiaozha@gmail.com>
2017-01-17 18:30:42 -0500

    modified partners page

--- partners.php
+++ partners.php
@@ -1,9 +1,10 @@
 <?php include('header.php'); ?>
-<h5>About</h5>
+<!-- <h5>About</h5> -->
 <div class="row">
   <div class="col-md-8">
     <div class="well">
-      <h1>Metropole Municipal Partners --<br> Active Research Collaborators</h1>
+      <h3>METROPOLE Municipal Partners -- Active Research Collaborators</h3>
+      <img alt="partners LOGO" src="image/partners.png" class="img-responsive" >
       <p>The integrated Metropole research framework was designed to reflect the
          practices and processes of a typical municipal planning project to
          assess local flooding risks and impacts.</p>
@@ -22,17 +23,29 @@
             also coordinated the outreach for the two workshops, and presented
             at and hosted two Metropole workshops.</p>
 
-            <a href="index.php"><img alt="METROPOLE LOGO" src="image/main_logo.png" style="width:300px;height:90px;"></a>
+
     </div>
 
-
-    <div class="well" >
+  </div>
+  <div class="col-md-4">
+    <div class="well">
       <p><em>METROPOLE partners included local elected and appointed officials and municipal staff with expertise in coastal engineering, environmental protection, sustainability, growth management and urban planning.</em></p>
-      <h2>American Planning Association – Florida Chapter</h2>
+
+    </div>
+  </div>
+</div>
+    <div class="well" >
+      <div class="row" >
+        <div class="col-sm-4  " >
+          <a href="http://www.floridaplanning.org"><img alt="APA FL LOGO" src="image/APA_FL_logo.png" style="width:200px;height:60px;"></a>
+        </div>
+        <div class="col-sm-8 " >
+          <span class="align-middle"><h2>American Planning Association – Florida Chapter</h2></span>
+        </div>
+      </div>
       <p>The Florida Chapter of the American Planning Association (APA Florida) is a non-profit organization of professionals, students, and allied professionals providing vision and leadership for the future development and redevelopment of Florida communities. APA Florida has over 2,600 members, making it the second largest APA Chapter in the country. Members of APA Florida work in the public and private sector; for Federal, state, and local government; in long-range planning and development review; in master planning, as well as site planning; in transportation planning, land use planning, environmental planning, school planning, design, and in many other capacities.</p>
 
       <p>As an NSF-funded partner in the Metropole project, APA Florida advised the research teams in the development of assessment tools and participated in US workshops. APA Florida is working with the scientists to adapt the research findings to support member and practitioner continuing education and co-develop educational materials and webinars in 2017.</p>
-      <a href="http://www.floridaplanning.org"><img alt="APA FL LOGO" src="image/APA_FL_logo.png" style="width:300px;height:90px;"></a>
     </div>
 
     <div class="well" id="partners">
@@ -40,33 +53,49 @@
 
       <div class="row" id="country">
         <div class="col-md-12">
-          <h3 id="us">Selsey, UK </h3>
+          <h3 id="us"><img alt="brazil_flag" src="image/brazil_flag.png" style="width:100px;height:60px;"> Santos, Brazil </h3>
         </div>
       </div>
-
-
-      <img alt="Selsey" src="image/Selsey_map.png" class="img-responsive">
-
-      <p>Selsey is a seaside town in the Chichester District of West Sussex, within the historic county of Sussex, England, at the southernmost point of the Manhood Peninsula, almost cut off from mainland by the sea. The Chichester District Council is responsible for maintenance of the seawall which protects the east side of Selsey, and has developed a Beach Management Plan for the region. To protect its business and the west side of Selsey, Bunn Leisure privately funded a £15m shoreline defence program in 2011. The sea wall is topped by a promenade, which is used extensively by the public and fishermen. It has a steep shingle beach and the groynes are short, steeply sloping and subject to heavy wave forces. The study area included 4,000 parcels located on the east side, which were primarily comprised of single and two story residential homes, and elder care facilities.</p>
+      <div class="row">
+        <div class="col-md-4">
+          <img alt="Santos" src="image/Santos_map.png" class="img-responsive">
+        </div>
+        <div class="col-md-8">
+          <p>The City of Santos is the main city in the Metropolitan Area of Santos (RMBS) in the State of São Paulo. The region is a strategic economic centre, with a large concentration of industries along the coastal zone and home to the largest port on South America’s Atlantic Coast.Tourism is also an important economic activity: some municipalities are among the most sought after areas as tourist destinations in Brazil. In contrast, the region has large deficits in infrastructure and about 16% of the population live on unstable slopes of the Serra do Mar, which are highly susceptible to landslides and floods. Santos is a portrait of the social asymmetry of the country, featuring upscale neighbourhoods, especially near the oceanfront and a few kilometres away are favelas and neighbourhoods where living conditions are very poor. Thus, the study focused on two zones – one in the Southeast and one in the Northwest, reflecting diverse socio-economic conditions and flood risks.</p>
+        </div>
+      </div>
 
       <div class="row" id="country">
         <div class="col-md-12">
-          <h3 id="us">Broward County, FL, US</h3>
+          <h3 id="us"><img alt="uk_flag" src="image/uk_flag.png" style="width:100px;height:60px;">Selsey, UK </h3>
         </div>
       </div>
-      <img alt="Miami" src="image/Miami_map.png" class="img-responsive">
-      <p>Located on the Atlantic Coast of Florida just north of Miami, Broward County has 1.8 million residents and attracts 11 million visitors annually. A major tourist destination, Broward has 31 municipalities and is bordered by the Atlantic Ocean to the east and the Everglades National Park to the west. Recognizing the increasing risks, Broward County, along with Miami-Dade, Monroe, and Palm Beach Counties formed The Southeast Florida Regional Climate Change Compact in 2010 to coordinate mitigation and adaptation activities across county lines. The Broward study area encompassed 10,000 parcels in the cities of Dania Beach, Fort Lauderdale and Hollywood. The area included a mix of properties from single family homes to high-rise condos and hotels, as well as several major regional assets, such as the Fort Lauderdale Airport.</p>
+
+      <div class="row">
+        <div class="col-md-4">
+          <img alt="Selsey" src="image/Selsey_map.png" class="img-responsive">
+        </div>
+        <div class="col-md-8">
+          <p>Selsey is a seaside town in the Chichester District of West Sussex, within the historic county of Sussex, England, at the southernmost point of the Manhood Peninsula, almost cut off from mainland by the sea. The Chichester District Council is responsible for maintenance of the seawall which protects the east side of Selsey, and has developed a Beach Management Plan for the region. To protect its business and the west side of Selsey, Bunn Leisure privately funded a £15m shoreline defence program in 2011. The sea wall is topped by a promenade, which is used extensively by the public and fishermen. It has a steep shingle beach and the groynes are short, steeply sloping and subject to heavy wave forces. The study area included 4,000 parcels located on the east side, which were primarily comprised of single and two story residential homes, and elder care facilities.</p>
+        </div>
+      </div>
+
 
       <div class="row" id="country">
         <div class="col-md-12">
-          <h3 id="us">Santos, Brazil </h3>
+          <h3 id="us"><img alt="us_flag" src="image/us_flag.png" style="width:100px;height:60px;">Broward County, FL, US</h3>
         </div>
       </div>
-      <img alt="Santos" src="image/Santos_map.png" class="img-responsive">
-      <p>The City of Santos is the main city in the Metropolitan Area of Santos (RMBS) in the State of São Paulo. The region is a strategic economic centre, with a large concentration of industries along the coastal zone and home to the largest port on South America’s Atlantic Coast.Tourism is also an important economic activity: some municipalities are among the most sought after areas as tourist destinations in Brazil. In contrast, the region has large deficits in infrastructure and about 16% of the population live on unstable slopes of the Serra do Mar, which are highly susceptible to landslides and floods. Santos is a portrait of the social asymmetry of the country, featuring upscale neighbourhoods, especially near the oceanfront and a few kilometres away are favelas and neighbourhoods where living conditions are very poor. Thus, the study focused on two zones – one in the Southeast and one in the Northwest, reflecting diverse socio-economic conditions and flood risks.</p>
+      <div class="row">
+        <div class="col-md-4">
+          <img alt="Miami" src="image/Miami_map.png" class="img-responsive">
+        </div>
+        <div class="col-md-8">
+          <p>Located on the Atlantic Coast of Florida just north of Miami, Broward County has 1.8 million residents and attracts 11 million visitors annually. A major tourist destination, Broward has 31 municipalities and is bordered by the Atlantic Ocean to the east and the Everglades National Park to the west. Recognizing the increasing risks, Broward County, along with Miami-Dade, Monroe, and Palm Beach Counties formed The Southeast Florida Regional Climate Change Compact in 2010 to coordinate mitigation and adaptation activities across county lines. The Broward study area encompassed 10,000 parcels in the cities of Dania Beach, Fort Lauderdale and Hollywood. The area included a mix of properties from single family homes to high-rise condos and hotels, as well as several major regional assets, such as the Fort Lauderdale Airport.</p>
+        </div>
+      </div>
     </div>
 
-  </div>
-</div>
+
 
 <?php include('footer.php'); ?>
