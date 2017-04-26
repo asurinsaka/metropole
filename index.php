93cd574b4fbe1ca3933a5ef161a4fc72ac2ce65a
Asurin <qiaozha@gmail.com>
2017-04-25 20:10:18 -0400

    Italicize the names of the publications:  Natural Hazard and Geoforum
    Please insert a paragraph break where you see the yellow line. That is a new section. Thank you.
    If you have questions please call me/see me.

--- index.php
+++ index.php
@@ -10,16 +10,20 @@
           <p>The international METROPOLE research project funded by the <a href="research.php#belmont">Belmont Forum</a> brought together <a href="team.php">social, economic, environmental scientists</a> and municipal practitioners and officials to assess the socio-economic and cultural factors which create potential barriers to adaptation, and influence <a href="research.php#aci">adaptive capacity</a> among local and regional institutions in three areas in Brazil, the UK and the US.</p>
           <p>Working with local decision makers, technical staff and stakeholder leaders in three study areas encompassing six cities, Metropole researchers evaluated the vulnerability to sea level rise and flooding with COAST and develop cost assessments of risk over time and developed costs-benefits analysis for chosen adaptation actions.</p>
           <p>The process observations and <a href="research.php#survey">survey results</a> are producing new insights relevant for local administrators, engineers and others involved in land-use planning. New perspectives on the links between values, type of infrastructure improvement and acceptability of public finance mechanisms will improve community support for large scale capital improvement projects.</p>
-          <strong>Read Our Latest Articles<strong>
-          <p><a href="https://link.springer.com/article/10.1007/s11069-017-2855-x" target="_blank">A Globally-Deployable Strategy for Co-Development of Adaptation Preferences to Sea-Level Rise: The Public Participation Case of Santos, Brazil</a>Natural Hazards, April 2017.</p>
-          <p><a href="http://www.sciencedirect.com/science/article/pii/S0016718517300507" target="_blank">Size does matter: city scale and the asymmetries of climate change adaptation in three coastal towns.</a>Geoforum Journal, January 2017.
-          <strong>Collaborations between universities, private sector and local government deliver tangible benefits to society – locally and globally.</strong>
         </div>
         <div class="col-md-3">
           <a href="files/Belmont Synthesis_20March.pdf"><img src="image/belmont_workshop.jpg" alt="Belmont Workshop" class="img-responsive"></a>
         </div>
       </div>
 
+      <div class="row">
+        <div class="col-md-12">
+          <p><strong>Read Our Latest Articles</strong></p>
+          <p><a href="https://link.springer.com/article/10.1007/s11069-017-2855-x" target="_blank">A Globally-Deployable Strategy for Co-Development of Adaptation Preferences to Sea-Level Rise: The Public Participation Case of Santos, Brazil</a>, <i>Natural Hazards</i>, April 2017.</p>
+          <p><a href="http://www.sciencedirect.com/science/article/pii/S0016718517300507" target="_blank">Size does matter: city scale and the asymmetries of climate change adaptation in three coastal towns</a>, <i>Geoforum Journal</i>, January 2017.</p>
+          <p><strong>Collaborations between universities, private sector and local government deliver tangible benefits to society – locally and globally.</strong></p>
+        </div>
+      </div>
   </div>
 
 
