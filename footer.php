a74af148893e29105c6f2279cb1e37c07404a71f
Asurin <qiaozha@gmail.com>
2017-05-16 19:20:40 -0400

    footer

--- footer.php
+++ footer.php
@@ -13,50 +13,54 @@
       </div>
     </div>
 
-    <div class="row">
+    <!-- <div class="row">
       <div class="col-md-12">
 
 
         <img class="img-responsive" alt="global footer" src="image/fund_global_footer.jpg" style="width:936px">
 
       </div>
-    </div>
-    <!-- old text footer -->
-
-    <!-- <div class="row" >
-      <div class="col-sm-4">
-        <a href="http://www.usf.edu"><img src="image/USF_logo.jpg" class="image-responsive" alt="USF_logo" style="width:200px;height:120px;"></a>
-        <p><b><a href="http://www.marine.usf.edu">College Of Marine Science</a></b></p>
-        <p><b><a href="http://imars.usf.edu">Institute for Marine Remote Sensing</a></b></p>
-
-      </div>
-      <div class=" col-sm-8">
-        <div class="row" id="sponsors">
-          <div class="col-md-3 col-sm-4 col-xs-6" id="img"><a href="https://belmontforum.org/funded-projects/integrated-framework-analyze-local-decision-making-and-adaptive-capacity-large-scale">
-            <img src="image/belmont.png" class="img-responsive" alt="Belmont Forum/G8 HORC sponsors" ></a>
-          </div>
-          <div class="col-md-2 col-sm-4 col-xs-6" id="img"><img src="image/nsf.png" class="img-responsive" alt="Responsive image" ></div>
-          <div class="col-md-2 col-sm-4 col-xs-6" id="img"><img src="image/fapesp.png" class="img-responsive" alt="Responsive image" ></div>
-          <div class="col-md-2 col-sm-4 col-xs-6" id="img"><img src="image/nerc.png" class="img-responsive" alt="Responsive image" ></div>
-          <div class="col-md-2 col-sm-4 col-xs-6" id="img"><img src="image/esrc.png" class="img-responsive" alt="Responsive image" ></div>
-        </div>
-        <div class="row top-buffer">
-          <div class="col-md-12">
-            <p><b>METROPOLE AGENCY SPONSORS</b></p>
-            <p>
-              <b>
-                Belmont Forum Call: Coastal Vulnerability</br>
-                NSF# G8MUREFU3FP-2201-040  </br>
-                FAPESP # 12/51876-0 </br>
-                METROPOLE website managed by the USF College of Marine Science IMARS </br>
-              </b>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      </div>
     </div> -->
+
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="row" id="sponsors">
+            <div class="col-md-3 col-sm-4 col-xs-6" id="img"><a href="https://belmontforum.org/funded-projects/integrated-framework-analyze-local-decision-making-and-adaptive-capacity-large-scale">
+              <img src="image/belmont.png" class="img-responsive" alt="Belmont Forum/G8 HORC sponsors" ></a>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6" id="img"><img src="image/nsf.png" class="img-responsive" alt="Responsive image" ></div>
+            <div class="col-md-2 col-sm-4 col-xs-6" id="img"><img src="image/fapesp.png" class="img-responsive" alt="Responsive image" ></div>
+            <div class="col-md-2 col-sm-4 col-xs-6" id="img"><img src="image/nerc.png" class="img-responsive" alt="Responsive image" ></div>
+            <div class="col-md-2 col-sm-4 col-xs-6" id="img"><img src="image/esrc.png" class="img-responsive" alt="Responsive image" ></div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <a href="http://www.usf.edu"><img src="image/usf-h-green.png" class="image-responsive" alt="USF_logo" style="width:333px;height:50px;"></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-8">
+          <!-- <p><b>METROPOLE AGENCY SPONSORS</b></p> -->
+          <p style="margin-left:16px; ">
+            <b>
+              </br>
+              Belmont Forum Call: Coastal Vulnerability</br>
+              NSF# G8MUREFU3FP-2201-040  </br>
+              FAPESP # 12/51876-0 </br>
+              <!-- METROPOLE website managed by the USF College of Marine Science IMARS </br> -->
+            </b>
+          </p>
+        </div>
+        <div class="col-md-4">
+          <b>
+            </br>
+            METROPOLE website managed by</br>
+            USF College of Marine Science,</br>
+            Institute for Marine Remote Sensing
+          </b>
+        </div>
+      </div>
+    </div>
   </footer>
 </body>
 </html>
